2cf9957529d4de2594caee25398756ea0fd8659c
michele <micheleepedrotti@gmail.com>
2021-03-11 16:24:57 +0100

    ingredients selection, total price and display in page

--- js/script.js
+++ js/script.js
@@ -13,4 +13,28 @@ calcBtn.addEventListener('click', function(){
         alert('Please give your burger a name')
     }
 
+    // variabile che richiama tutti i checkbox
+    var checkboxList = document.getElementsByClassName('whichIngr');
+    // costo panino di partenza
+    var totalPrice = 10;
+
+    // vado a cercare i checbox attivi
+    for (var i=0; i<checkboxList.length; i++){
+
+        // variabile per i checkbox attvi/non attivi
+        var isChecked = checkboxList[i].checked;
+        //  var per il costo di ogni ingediente
+        var ingrCost = parseInt(checkboxList[i].dataset.cost);
+
+        // se checkbox attivo aggiungo al prezzo di base il costo dell'ingrediente
+        if (isChecked){
+            totalPrice += ingrCost;
+        }
+    }
+
+    // stampo il prezzo nel footer
+    var yourPrice = document.getElementById('yourPrice');
+    yourPrice.innerHTML = totalPrice;
+
+
 });
